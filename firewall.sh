9ac2165521af422eea1be177916889a1523fe6a7
Omar Vasquez <omar@vasquezlma.com>
2020-12-25 09:06:03 +0000

    Changed sha-bang

--- firewall.sh
+++ firewall.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 sudo iptables -A INPUT -p tcp --dport 1337 -m state --state  NEW -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 1338 -m state --state  NEW -j ACCEPT
